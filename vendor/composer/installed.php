44f27901001d98f3c869b6e1914178e9b4f30cb4
usuario <usuario@ubuntu-22.04>
2024-10-28 17:49:51 +0100

    uploading some corrections

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7ed1fe6bad10d7b76a40dab3b141cc307ffab55a',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7ed1fe6bad10d7b76a40dab3b141cc307ffab55a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
